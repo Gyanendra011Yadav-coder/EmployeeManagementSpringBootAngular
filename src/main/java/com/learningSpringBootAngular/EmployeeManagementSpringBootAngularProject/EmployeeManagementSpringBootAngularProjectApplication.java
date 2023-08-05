ccb2495c6e9da4631b6327b14b5edf5a52f430b2
Gyanendra-Yadav <gyanendra.yadav@deltatech.gg>
2023-08-05 17:05:53 +0530

    GIT Init

---
+++ src/main/java/com/learningSpringBootAngular/EmployeeManagementSpringBootAngularProject/EmployeeManagementSpringBootAngularProjectApplication.java
@@ -0,0 +1,13 @@
+package com.learningSpringBootAngular.EmployeeManagementSpringBootAngularProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeManagementSpringBootAngularProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeManagementSpringBootAngularProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/learningSpringBootAngular/EmployeeManagementSpringBootAngularProject/EmployeeManagementSpringBootAngularProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learningSpringBootAngular.EmployeeManagementSpringBootAngularProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeManagementSpringBootAngularProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
